3314cb832d7cf5f7311e091c941ffb715b49a8f6
MinexAutomation <minex.automation@gmail.com>
2019-11-29 00:30:12 -0800

    Added email sender service definition.

---
+++ source/R5T.Emden.Base/Code/Services/Interfaces/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Mail;
+
+
+namespace R5T.Emden
+{
+    /// <summary>
+    /// A service that sends emails.
+    /// </summary>
+    public interface IEmailSender
+    {
+        void Send(MailMessage message);
+    }
+}
