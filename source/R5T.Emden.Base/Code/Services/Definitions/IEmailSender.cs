41d9fbb066b6e80674b489cf8fededd0fed2c17d
davidcoats <david@getsafetycone.com>
2022-04-27 15:30:50 -0700

    Interim changes.

---
+++ source/R5T.Emden.Base/Code/Services/Definitions/IEmailSender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+using R5T.T0064;
+
+
+namespace R5T.Emden
+{
+    /// <summary>
+    /// A service that sends emails.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface IEmailSender : IServiceDefinition
+    {
+        //HACK: Will fill in the "From" address.
+        void Send(MailMessage message);
+
+        Task SendAsync(MailMessage message);
+    }
+}
